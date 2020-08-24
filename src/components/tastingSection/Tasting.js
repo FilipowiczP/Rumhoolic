5ea9cfc0178869649515cc66e4d8ed7a83822e0a
FilipowiczP <przemyslaw.filipowicz1@gmail.com>
2020-08-24 11:22:05 +0200

    Add desktop style for tasting section

--- src/components/tastingSection/Tasting.js
+++ src/components/tastingSection/Tasting.js
@@ -16,7 +16,7 @@ const Tasting = () =>{
             
         }, 2000);
         return clearInterval(intervalID);
-    })
+    },[])
 
     return(
         <section id="tasting" className="tasting">
@@ -24,14 +24,14 @@ const Tasting = () =>{
             {tastingContent.map(({id, fotos, text}) =>{
                 return (
                     <div className="tasting__content" key={id} data-tasting={id}>
+                        <h1 className="tasting__title">{text.name}</h1>
                         <div className="tasting__content__box">
                             <img className="tasting__foto" src={fotos[slider]}/>
                         </div>
-                        <h1 className="tasting__title">{text.name}</h1>
                         <p className="tasting__text"><span className="tasting__text__heading">Miej<span className="tasting__color">sce:</span></span> {text.place}</p>
                         <p className="tasting__text"><span className="tasting__text__heading">Prowadz<span className="tasting__color">oący:</span></span> {text.leader}</p>
-                        <p className="tasting__text"><span className="tasting__text__heading">Degustowane <span className="tasting__color">rumy:</span></span> {text.rums}</p>
-                        <p className="tasting__text"><span className="tasting__text__heading">Moja <span className="tasting__color">opinia:</span></span> {text.opinion}</p>
+                        <p className="tasting__text"><span className="tasting__text__heading">Degustowane  <span className="tasting__color">rumy:</span></span> {text.rums}</p>
+                        <p className="tasting__text"><span className="tasting__text__heading">Moja  <span className="tasting__color">opinia:</span></span> {text.opinion}</p>
                     </div>
                 )
             })}
