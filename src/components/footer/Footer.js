3b83a0d7b03961c4f9e96e9b16dbacb1bb3d144e
FilipowiczP <przemyslaw.filipowicz1@gmail.com>
2020-08-26 12:07:43 +0200

    Created footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./footer.scss";
+import instagram from "./images/instagram1.png";
 
 const Footer = () =>{
 
     return(
-        <footer id="footer" className="footer"></footer>
+        <footer id="footer" className="footer">
+            <h1 className="footer__text">Contact:</h1>
+            <h2 className="footer__text">Mateusz Makuch</h2>
+            <h2 className="footer__text">e-mail: mateusz.s.makuch@outlook.com</h2>
+            <a href="https://www.instagram.com/rumhoolic/?hl=pl" className="instagram"><img src={instagram} alt="instagram" className="instagram__foto" /> Instagram @rumhoolic</a>
+        </footer>
     )
 }
 
