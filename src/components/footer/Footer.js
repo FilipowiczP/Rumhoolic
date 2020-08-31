f3993acaa332afebcfcc425537379689ee6ba535
FilipowiczP <przemyslaw.filipowicz1@gmail.com>
2020-08-31 16:49:05 +0200

    Changed instagram in footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,7 +9,10 @@ const Footer = () =>{
             <h1 className="footer__text">Contact:</h1>
             <h2 className="footer__text">Mateusz Makuch</h2>
             <h2 className="footer__text">e-mail: mateusz.s.makuch@outlook.com</h2>
-            <a href="https://www.instagram.com/rumhoolic/?hl=pl" className="instagram"><img src={instagram} alt="instagram" className="instagram__foto" /> Instagram @rumhoolic</a>
+            <div className="footer__box">
+                <a href="https://www.instagram.com/rumhoolic/?hl=pl" className="instagram"><img src={instagram} alt="instagram" className="instagram__foto" /></a>
+                <p className="footer__box__text">Instagram @rumhoolic</p>
+            </div>
         </footer>
     )
 }
